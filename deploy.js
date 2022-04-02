737478b865bc8f9d6cd816659cafe508f0b113b0
AliErcanOzgokce <78970916+AliErcanOzgokce@users.noreply.github.com>
2022-04-02 17:41:42 +0300

    adding test file

--- deploy.js
+++ deploy.js
@@ -19,6 +19,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
